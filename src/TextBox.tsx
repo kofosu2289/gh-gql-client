d74115edf0dfe2c4a597aa8057c7df7111e10e8f
Kenneth Ofosu <kofosu89@gmail.com>
2020-10-16 17:59:57 -0400

    Create TextBox component

---
+++ src/TextBox.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useRef, useEffect } from "react";
+
+type TextBoxProps = {
+  top?: string | number;
+  bottom?: string | number;
+  left?: string | number;
+  onSubmit(): void;
+};
+
+export const TextBox: FC<TextBoxProps> = ({ onSubmit, ...rest }) => {
+  return (
+    <blessed-textbox
+      height={1}
+      style={{
+        bg: "white",
+        fg: "black",
+      }}
+      keys
+      inputOnFocus
+      onSubmit={onSubmit}
+      {...rest}
+    />
+  );
+};
