5fa3351a4f3ecda5a030a35cb34814d6543b94a4
Kenneth Ofosu <kofosu89@gmail.com>
2020-10-20 16:57:59 -0400

    Create NewPullRequestSuccess component

--- src/PullRequests/NewPullRequest.tsx
+++ src/PullRequests/NewPullRequest.tsx
@@ -1,17 +1,20 @@
-import { gql } from "apollo-boost"
-import React, { useState } from "react"
-import { useApolloClient, useMutation } from "react-apollo-hooks"
-import { Field } from "../Field"
-import { Form, FormValues } from "../Form"
-import { Panel } from "../Panel"
+import { gql } from "apollo-boost";
+import React, { useState } from "react";
+import { useApolloClient, useMutation } from "react-apollo-hooks";
+import { Field } from "../Field";
+import { Form, FormValues } from "../Form";
+import { Panel } from "../Panel";
 import {
   createNewPullRequest,
   createNewPullRequestVariables,
-  createNewPullRequest_createPullRequest_pullRequest
-} from "./types/createNewPullRequest"
-import { getRepository, getRepositoryVariables } from "../queries/types/getRepository"
-import { GET_REPOSITORY } from "../queries/getRepository"
-import { NewPullRequestSuccess } from "./NewPullRequestSuccess"
+  createNewPullRequest_createPullRequest_pullRequest,
+} from "./types/createNewPullRequest";
+import {
+  getRepository,
+  getRepositoryVariables,
+} from "../queries/types/getRepository";
+import { GET_REPOSITORY } from "../queries/getRepository";
+import { NewPullRequestSuccess } from "./NewPullRequestSuccess";
 
 const CREATE_PULL_REQUEST = gql`
   mutation createNewPullRequest(
@@ -36,37 +39,34 @@ const CREATE_PULL_REQUEST = gql`
       }
     }
   }
-`
+`;
 
 export const NewPullRequest = () => {
   const [
     pullRequest,
-    setPullRequest
-  ] = useState<createNewPullRequest_createPullRequest_pullRequest | null>()
+    setPullRequest,
+  ] = useState<createNewPullRequest_createPullRequest_pullRequest | null>();
   const [createPullRequest] = useMutation<
     createNewPullRequest,
     createNewPullRequestVariables
-  >(CREATE_PULL_REQUEST)
+  >(CREATE_PULL_REQUEST);
 
-  const client = useApolloClient()
+  const client = useApolloClient();
 
   const onSubmit = async (values: FormValues) => {
-    const [repo, title, body, baseRefName, headRefName] = values.textbox
-    const [owner, name] = repo.split("/")
+    const [repo, title, body, baseRefName, headRefName] = values.textbox;
+    const [owner, name] = repo.split("/");
 
-    const { data } = await client.query<
-      getRepository,
-      getRepositoryVariables
-    >({
+    const { data } = await client.query<getRepository, getRepositoryVariables>({
       query: GET_REPOSITORY,
       variables: {
         owner,
-        name
-      }
-    })
+        name,
+      },
+    });
 
     if (!data || !data.repository) {
-      return
+      return;
     }
 
     const result = await createPullRequest({
@@ -75,15 +75,15 @@ export const NewPullRequest = () => {
         body,
         repositoryId: data.repository.id,
         baseRefName,
-        headRefName
-      }
-    })
+        headRefName,
+      },
+    });
 
-    setPullRequest(result.data?.createPullRequest?.pullRequest)
-  }
+    setPullRequest(result.data?.createPullRequest?.pullRequest);
+  };
 
   if (pullRequest) {
-    return <NewPullRequestSuccess pullRequest={pullRequest} />
+    return <NewPullRequestSuccess pullRequest={pullRequest} />;
   }
 
   return (
@@ -98,33 +98,13 @@ export const NewPullRequest = () => {
         {(triggerSubmit) => {
           return (
             <>
-              <Field
-                top={0}
-                label="Repo: "
-                onSubmit={triggerSubmit}
-              />
-              <Field
-                top={1}
-                label="Title: "
-                onSubmit={triggerSubmit}
-              />
-              <Field
-                top={2}
-                label="Body: "
-                onSubmit={triggerSubmit}
-              />
-              <Field
-                top={3}
-                label="Base: "
-                onSubmit={triggerSubmit}
-              />
-              <Field
-                top={4}
-                label="Head: "
-                onSubmit={triggerSubmit}
-              />
+              <Field top={0} label="Repo: " onSubmit={triggerSubmit} />
+              <Field top={1} label="Title: " onSubmit={triggerSubmit} />
+              <Field top={2} label="Body: " onSubmit={triggerSubmit} />
+              <Field top={3} label="Base: " onSubmit={triggerSubmit} />
+              <Field top={4} label="Head: " onSubmit={triggerSubmit} />
             </>
-          )
+          );
         }}
       </Form>
       <blessed-text
@@ -142,5 +122,5 @@ export const NewPullRequest = () => {
         content="Enter: Submit"
       />
     </Panel>
-  )
-}
+  );
+};
